76564b994fe61030020536b9af468f6c31745551
bsyyl <zixuan.zhu0428@gmail.com>
2026-04-07 16:26:03 +0800

    update

--- ship_report/Mind2Report-main/src/agent/agent.py
+++
@@ -1,6 +0,0 @@
-from .supervisor_graph import build_supervisor_graph
-
-
-def build_agent():
-    """Build and return the multi-agent supervisor graph."""
-    return build_supervisor_graph()
--- ship_report/Mind2Report-main/src/prompts/__init__.py
+++
@@ -1,5 +0,0 @@
-from .template import apply_prompt_template
-
-__all__ = [
-    "apply_prompt_template",
-]
